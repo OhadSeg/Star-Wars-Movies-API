6e07ee855a9027619f12079319096e98a092499a
Ohad Segal <segalohad@gmail.com>
2023-05-12 02:45:27 +0300

    Add Error handling for fetch movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
-import { RotatingLines } from  'react-loader-spinner'
+import { RotatingLines } from "react-loader-spinner";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const[isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setError(null);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw new Error();
+      }
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   }
 
@@ -29,9 +47,12 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-       {isLoading && movies.length > 0 && <RotatingLines strokeColor="gold"/>}
-       {!isLoading && movies.length === 0 && <p>No movies were fetched yet.</p> }
-       {!isLoading && <MoviesList movies={movies} />} 
+        {!isLoading && movies.length === 0 && !(error) &&(
+          <p>No movies were fetched yet.</p>
+        )}
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && error && <p> {error} </p>}
+        {isLoading && movies.length > 0 && <RotatingLines strokeColor="gold" />}
       </section>
     </React.Fragment>
   );
